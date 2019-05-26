9b83a3e6e73c649187b49e2f6ba1477a7b33e7c3
yejie0_0 <37886572+yejie00@users.noreply.github.com>
2019-05-26 14:22:30 +0800

    Create prepare.js

---
+++ js/client/prepare.js
@@ -0,0 +1,29 @@
+var prepare ={};
+prepare.init = function(){
+    game.add.plugin(Fabrique.Plugins.InputField);
+};
+prapare.create = function(){
+    game.load.image('pre_background','assets/pic/pre_background.jpg')
+    game.load.audio('*****','assets/music/*****')
+    
+
+
+
+
+
+
+
+};
+prepare.display_input = function(){
+
+
+
+
+
+};
+prepare.display_button = function(){
+
+
+
+
+};
