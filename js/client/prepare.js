1de366e953efad5d52cb86f3d397ca841d92d93c
yejie0_0 <37886572+yejie00@users.noreply.github.com>
2019-05-26 16:46:38 +0800

    游戏准备第一次提交

--- js/client/prepare.js
+++ js/client/prepare.js
@@ -2,28 +2,26 @@ var prepare ={};
 prepare.init = function(){
     game.add.plugin(Fabrique.Plugins.InputField);
 };
-prapare.create = function(){
+
+prepare.preload = function(){
     game.load.image('pre_background','assets/pic/pre_background.jpg')
-    game.load.audio('*****','assets/music/*****')
-    
-
-
-
-
-
-
-
+    game.load.image('start_pre','assets/pic/start_pre.png');
+    game.load.spritesheet('bubble', 'assets/sprites/prompt.png',5,5);
 };
+
+prepare.create = function(){
+    Login.bg = game.add.tileSprite(0, 0, 1920, 1080, 'pre_background');
+    Login.display_input();
+    Login.display_button();
+};
+
 prepare.display_input = function(){
 
-
-
-
-
 };
+
 prepare.display_button = function(){
-
-
-
-
+    prepare.login_button = game.add.button(1200,500,'start_pre',Client.send_log);
+    game.state.start('prepare')
 };
+
+
