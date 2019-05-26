2fa92198e6a20a43cc021a50f3d676f8845bfe77
XlDerek <1615858248@qq.com>
2019-05-26 15:30:01 +0800

    add bgm and input areas

--- js/client/Information.js
+++ js/client/Information.js
@@ -5,18 +5,22 @@ Information.init = function(){
 };
 
 Information.preload = function(){
-    game.load.audio()
+    game.load.audio('information_bgm','assets/music/information_bgm')
     game.load.image('information_background','assets/pic/background3.jpg');
+    game.load.image
+    game.load.image
 };
 
 Information.create = function(){
-    Information.music = game.add.audio
+    Information.music = game.add.audio('information_bgm');
     Information.music.play();
     Information.bg = game.add.tileSprite(0,0,1920.1080,'informaiton_background');
+    Information.display_input();
+    Information.display_button();
 };
 
 Information.display_input = function(){
-    Information.userid_input = game.add.inputField(1200,300,{
+    Information.username_input = game.add.inputField(1200,300,{
         width: 300,
         padding: 10,
         fill: '#000',
@@ -30,6 +34,69 @@ Information.display_input = function(){
         placeHolderColor: '#b2af9b',
         cursorColor: '#b2af9b',
         max: 20
-    });
-    Information.userintro_input = game.add.inputField();
+    });                                                                 //显示位置暂未修改
+    Information.userrealname_input = game.add.inputField(1200,300,{
+        width: 300,
+        padding: 10,
+        fill: '#000',
+        stroke: '#fff',
+        backgroundColor: '#d0cdba',
+        borderWidth: 2,
+        borderColor: '#b2af9b',
+        borderRadius: 3,
+        font: '18px pixel',
+        placeHolder: 'Your username',
+        placeHolderColor: '#b2af9b',
+        cursorColor: '#b2af9b',
+        max: 20
+    });                                                                 //显示位置暂未修改
+    Information.userintro_input = game.add.inputField(1200,300,{
+        width: 300,
+        padding: 10,
+        fill: '#000',
+        stroke: '#fff',
+        backgroundColor: '#d0cdba',
+        borderWidth: 2,
+        borderColor: '#b2af9b',
+        borderRadius: 3,
+        font: '18px pixel',
+        placeHolder: 'Your username',
+        placeHolderColor: '#b2af9b',
+        cursorColor: '#b2af9b',
+        max: 20
+    });                                                                 //显示位置暂未修改
+    Information.userbirth_input = game.add.inputField(1200,300,{
+        width: 300,
+        padding: 10,
+        fill: '#000',
+        stroke: '#fff',
+        backgroundColor: '#d0cdba',
+        borderWidth: 2,
+        borderColor: '#b2af9b',
+        borderRadius: 3,
+        font: '18px pixel',
+        placeHolder: 'Your username',
+        placeHolderColor: '#b2af9b',
+        cursorColor: '#b2af9b',
+        max: 20
+    });                                                                 //显示位置暂未修改
+    Information.usergender_input = game.add.inputField(1200,300,{
+        width: 300,
+        padding: 10,
+        fill: '#000',
+        stroke: '#fff',
+        backgroundColor: '#d0cdba',
+        borderWidth: 2,
+        borderColor: '#b2af9b',
+        borderRadius: 3,
+        font: '18px pixel',
+        placeHolder: 'Your username',
+        placeHolderColor: '#b2af9b',
+        cursorColor: '#b2af9b',
+        max: 20
+    });                                                                 //显示位置暂未修改
+};
+
+Information.display_button = function(){
+    Information.save_button = game.add.button
 };
